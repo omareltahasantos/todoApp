886350fc0457e1c1fc3294704a8f72e0de422b67
omareltahasantos <omareltahasan6@gmail.com>
2021-09-04 23:50:49 +0200

    change vuetify text font size

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@
 
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+      <v-app-bar-title class="text-h5">Vuetify Todo</v-app-bar-title>
 
       <v-spacer></v-spacer>
       
